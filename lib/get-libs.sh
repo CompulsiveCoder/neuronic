5a33cc3dcbdf8128d7aa3632379f2afe16f5ba0c
CompulsiveCoder <compulsivecoder@gmail.com>
2016-07-27 16:10:58 +1000

    Added missing files

---
+++ lib/get-libs.sh
@@ -0,0 +1,13 @@
+echo "Getting library files"
+echo "  Dir: $PWD"
+
+NUGET_FILE="nuget.exe"
+
+if [ ! -f "$NUGET_FILE" ];
+then
+    wget http://nuget.org/nuget.exe
+fi
+
+mono nuget.exe install newtonsoft.json -version 8.0.3
+mono nuget.exe install nunit -version 2.6.4
+mono nuget.exe install nunit.runners -version 2.6.4
